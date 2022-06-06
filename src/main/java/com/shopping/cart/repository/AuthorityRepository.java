f8d06d79c2279a542987e7e105777a4d8193caba
Prateek Patel <prateek.patel@divergentsl.com>
2022-06-06 15:28:09 +0530

    Initial version of jhipster generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/shopping/cart/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.shopping.cart.repository;
+
+import com.shopping.cart.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/shopping/cart/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.shopping.cart.service.dto;
